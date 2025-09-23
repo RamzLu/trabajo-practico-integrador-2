e474a42d25313a042346730e54ff3a1bb68daef9
Ramirez Luana <luanaabigail168@gmail.com>
2025-09-23 10:50:16 -0300

    middleware para verificar el token

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+export const validateToken = (req, res, next) => {
+  const token = req.cookies.token;
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userLog = decoded;
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      msg: "Token no válido, debes logearte.",
+    });
+  }
+};
