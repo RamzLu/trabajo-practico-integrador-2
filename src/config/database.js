efc45bbf9cdb125f8eb1e9654bb07a6f4fc85136
Ramirez Luana <luanaabigail168@gmail.com>
2025-09-23 10:50:03 -0300

    base de datos

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const conectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_UR);
+    // await mongoose.connection.dropDatabase();
+    console.log("DB conectada correctamente.");
+  } catch (error) {
+    console.log("No se pudo conectar a la BD", error);
+  }
+};
