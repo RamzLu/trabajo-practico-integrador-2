851230fd2ea9379bdf77620169b57104174960df
Ramirez Luana <luanaabigail168@gmail.com>
2025-09-24 11:42:52 -0300

    acceso a las rutas de articles

--- src/routes/article.route.js
+++ src/routes/article.route.js
@@ -8,11 +8,23 @@ import {
   updateArticle,
 } from "../controllers/article.controller.js";
 import { validateToken } from "../middlewares/authMiddleware.js";
+import { ownerOrAdmin } from "../middlewares/ownerOrAdminMiddleware.js";
+import { ArticleModel } from "../models/article.model.js";
 export const routeArticle = Router();
 
 routeArticle.get("/articles/my", validateToken, getUserLogArticles);
+routeArticle.post("/articles", validateToken, createArticle);
 routeArticle.get("/articles", validateToken, getAllArticles);
 routeArticle.get("/articles/:id", validateToken, getArticleById);
-routeArticle.post("/articles", validateToken, createArticle);
-routeArticle.delete("/articles/:id", validateToken, deleteArticle);
-routeArticle.put("/articles/:id", validateToken, updateArticle);
+routeArticle.put(
+  "/articles/:id",
+  validateToken,
+  ownerOrAdmin(ArticleModel),
+  updateArticle
+);
+routeArticle.delete(
+  "/articles/:id",
+  validateToken,
+  ownerOrAdmin(ArticleModel),
+  deleteArticle
+);
