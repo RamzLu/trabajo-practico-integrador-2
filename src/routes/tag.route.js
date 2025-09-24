326e2d40da1bbb08acbf963413ad02000f70cf87
Ramirez Luana <luanaabigail168@gmail.com>
2025-09-24 11:14:34 -0300

    acceso a las rutas de tag

--- src/routes/tag.route.js
+++ src/routes/tag.route.js
@@ -7,11 +7,12 @@ import {
   updateTag,
 } from "../controllers/tag.controller.js";
 import { validateToken } from "../middlewares/authMiddleware.js";
+import { authAdmin } from "../middlewares/adminMiddleware.js";
 
 export const tagRouter = Router();
 
-tagRouter.post("/tags", validateToken, createTag);
+tagRouter.post("/tags", validateToken, authAdmin, createTag);
 tagRouter.get("/tags", validateToken, getTags);
-tagRouter.delete("/tags/:id", validateToken, deleteTag);
-tagRouter.put("/tags/:id", validateToken, updateTag);
 tagRouter.get("/tags/:id", validateToken, getTagById);
+tagRouter.put("/tags/:id", validateToken, authAdmin, updateTag);
+tagRouter.delete("/tags/:id", validateToken, authAdmin, deleteTag);
