2629be2207ee89fd1384119048f25990e44d87ec
Ramirez Luana <luanaabigail168@gmail.com>
2025-09-24 11:13:22 -0300

    acceso a las rutas de user solo admin

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -6,11 +6,11 @@ import {
   updateUser,
 } from "../controllers/user.controller.js";
 import { validateToken } from "../middlewares/authMiddleware.js";
-import { authAdmin } from "../middlewares/authAdminMiddleware.js";
+import { authAdmin } from "../middlewares/adminMiddleware.js";
 
 export const routeUser = Router();
 
-routeUser.delete("/users/:id", validateToken, deleteUser);
+routeUser.delete("/users/:id", validateToken, authAdmin, deleteUser);
 routeUser.get("/users", validateToken, authAdmin, getAllUsers);
-routeUser.get("/users/:id", validateToken, getUserById);
-routeUser.put("/users/:id", validateToken, updateUser);
+routeUser.get("/users/:id", validateToken, authAdmin, getUserById);
+routeUser.put("/users/:id", validateToken, authAdmin, updateUser);
