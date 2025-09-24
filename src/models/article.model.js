7203fa03f97d9990a2941415f441e24b1d70c3cb
Ramirez Luana <luanaabigail168@gmail.com>
2025-09-24 15:25:30 -0300

    hice un campo virtual de comment

--- src/models/article.model.js
+++ src/models/article.model.js
@@ -3,46 +3,52 @@ import { UserModel } from "./user.model.js";
 import { TagModel } from "./tag.model.js";
 import { CommentModel } from "./comment.model.js";
 
-const articleSchema = new Schema({
-  title: {
-    type: String,
-    minlength: 3,
-    maxlength: 200,
-    required: true,
-  },
-  content: {
-    type: String,
-    minlength: 10,
-    required: true,
-  },
-  excerpt: {
-    type: String,
-    minlength: 10,
-  },
-  status: {
-    type: String,
-    enum: ["published", "archived"],
-    default: "published",
-  },
-  //* RELACIÓN 1:N con User
-  author: {
-    type: Types.ObjectId,
-    ref: "User",
-    required: true,
-    validate: {
-      validator: async function (value) {
-        const existAuhtor = await UserModel.findById(value);
-        return !!existAuhtor;
+const articleSchema = new Schema(
+  {
+    title: {
+      type: String,
+      minlength: 3,
+      maxlength: 200,
+      required: true,
+    },
+    content: {
+      type: String,
+      minlength: 10,
+      required: true,
+    },
+    excerpt: {
+      type: String,
+      minlength: 10,
+    },
+    status: {
+      type: String,
+      enum: ["published", "archived"],
+      default: "published",
+    },
+    //* RELACIÓN 1:N con User
+    author: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+      validate: {
+        validator: async function (value) {
+          const existAuhtor = await UserModel.findById(value);
+          return !!existAuhtor;
+        },
+        message: "El autor referenciado no existe.",
       },
-      message: "El autor referenciado no existe.",
+    },
+    //* RELACIÓN N:M con Tag
+    tags: {
+      type: [Types.ObjectId],
+      ref: "Tag",
     },
   },
-  //* RELACIÓN N:M con Tag
-  tags: {
-    type: [Types.ObjectId],
-    ref: "Tag",
-  },
-});
+  {
+    versionKey: false,
+    toJSON: { virtuals: true },
+  }
+);
 
 articleSchema.post("findOneAndDelete", async function (doc) {
   // doc es el articulo borrado
@@ -55,4 +61,9 @@ articleSchema.post("findOneAndDelete", async function (doc) {
   }
 });
 
+articleSchema.virtual("comments", {
+  ref: "Comment",
+  localField: "_id",
+  foreignField: "article",
+});
 export const ArticleModel = model("Article", articleSchema);
